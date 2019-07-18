ada6079a14fe0ba4e22e3130800bf299fe627575
beamguide <46237424+beamguide@users.noreply.github.com>
2019-07-18 15:55:11 +0700

    adapted syberia logo
    credit goes to syberia rom

--- packages/SystemUI/src/com/android/systemui/tuner/StatusbarItems.java
+++ packages/SystemUI/src/com/android/systemui/tuner/StatusbarItems.java
@@ -14,14 +14,42 @@
 
 package com.android.systemui.tuner;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v14.preference.PreferenceFragment;
 import com.android.systemui.R;
 
 public class StatusbarItems extends PreferenceFragment {
+    
+    private static final String KEY_STATUS_BAR_LOGO = "status_bar_logo";
+
+	private SwitchPreference mShowAncientLogo;
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mShowAncientLogo = (SwitchPreference) findPreference(KEY_STATUS_BAR_LOGO);
+        mShowAncientLogo.setChecked((Settings.System.getInt(resolver, Settings.System.STATUS_BAR_LOGO, 1) == 1));
+    }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.statusbar_items);
+    }
+    
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if  (preference == mShowAncientLogo) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.STATUS_BAR_LOGO, checked ? 1:0);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 }
